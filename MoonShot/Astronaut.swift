c2c1998c98a923ceff98a0b76460dd5e89f4514f
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-07 19:20:34 +0200

    Loading data
    - add astronauts.json with data about Apollo Program's  astronauts
    - add missions.json with data about Apollo Program's missions
    - add images into assets
    
    Astronaut
    - create data Model
    
    Bundle-Decodable
    - create Bundle extension  for decoding data from json

---
+++ MoonShot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot
+//
+//  Created by HubertMac on 07/10/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
