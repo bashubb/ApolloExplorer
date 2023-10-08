a8e7fe275b421728bb8687132f5005f5db9a0ce5
Hubert Kielkowski <hubert.kielkowski@gmail.com>
2023-10-08 14:02:36 +0200

    MissionView - add launch date below the mission badge - extract CrewView - extract dividers
    ContentView
    - add toolbar button for changing display Grids - List

--- MoonShot/ContentView.swift
+++ MoonShot/ContentView.swift
@@ -8,59 +8,136 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isListView = false
+    
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
     
-    let columns = [
-        GridItem(.adaptive(minimum: 150))
-    ]
+    
     
     
     var body: some View {
         NavigationStack {
             ScrollView {
-                LazyVGrid(columns: columns) {
-                    ForEach(missions) {mission in
-                        NavigationLink {
-                            MissionView(mission: mission, astronauts: astronauts)
-                        } label: {
-                            VStack {
-                                Image(mission.image)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .frame(width: 100, height: 100)
-                                    .padding()
-                                VStack {
-                                    Text(mission.displayName)
-                                        .font(.headline)
-                                        .foregroundStyle(Color.white)
-                                    
-                                    Text(mission.formattedLaunchDate)
-                                        .font(.caption)
-                                        .foregroundStyle(Color.white.opacity(0.5))
-                                }
-                                .padding(.vertical)
-                                .frame(maxWidth: .infinity)
-                                .background(.lightBackground)
-                            }
-                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                            .overlay(
-                                RoundedRectangle(cornerRadius: 10)
-                                    .stroke(.lightBackground)
-                            )
-                        }
-                        
-                    }
+                
+                if !isListView {
+                    GridView(missions: missions, astronauts:astronauts)
+                } else {
+                    ListView(missions: missions, astronauts: astronauts)
                 }
-                .padding([.horizontal, .bottom])
             }
             .navigationTitle("Moonshot")
             .background(.darkBackground)
             .preferredColorScheme(.dark)
+            .toolbar {
+                Button {
+                    isListView.toggle()
+                } label: {
+                    HStack(spacing: 2) {
+                        Image(systemName: "square.grid.2x2")
+                        Image(systemName: "list.bullet")
+                    }
+                    .foregroundStyle(Color.white)
+                    
+                }
+        }
         }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+
+// Grid
+struct GridView: View {
+    
+    let missions: [Mission]
+    let astronauts: [String: Astronaut]
+    
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
+    
+    var body: some View {
+        LazyVGrid(columns: columns) {
+            ForEach(missions) {mission in
+                NavigationLink {
+                    MissionView(mission: mission, astronauts: astronauts)
+                } label: {
+                    VStack {
+                        Image(mission.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .padding()
+                        VStack {
+                            Text(mission.displayName)
+                                .font(.headline)
+                                .foregroundStyle(Color.white)
+                            
+                            Text(mission.formattedLaunchDate)
+                                .font(.caption)
+                                .foregroundStyle(Color.white.opacity(0.5))
+                        }
+                        .padding(.vertical)
+                        .frame(maxWidth: .infinity)
+                        .background(.lightBackground)
+                    }
+                    .clipShape(RoundedRectangle(cornerRadius: 10))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 10)
+                            .stroke(.lightBackground)
+                    )
+                }
+                
+            }
+        }
+        .padding([.horizontal, .bottom])
+    }
+}
+
+
+
+// List
+struct ListView: View {
+    
+    let missions: [Mission]
+    let astronauts: [String: Astronaut]
+    
+    var body: some View {
+        
+        ForEach(missions) {mission in
+            NavigationLink {
+                MissionView(mission: mission, astronauts: astronauts)
+            } label: {
+                HStack {
+                    Image(mission.image)
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 100, height: 100)
+                        .padding()
+                    VStack {
+                        Text(mission.displayName)
+                            .font(.headline)
+                            .foregroundStyle(Color.white)
+                        
+                        Text(mission.formattedLaunchDate)
+                            .font(.caption)
+                            .foregroundStyle(Color.white.opacity(0.5))
+                    }
+                    .frame(maxWidth: .infinity, maxHeight: .infinity)
+                    .background(.lightBackground)
+                }
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(.lightBackground)
+                )
+            }
+            
+        }
+        
+    }
 }
--- MoonShot/MissionView.swift
+++ MoonShot/MissionView.swift
@@ -26,67 +26,31 @@ struct MissionView: View {
                         .frame(maxWidth: geo.size.width * 0.6)
                         .padding(.top)
                     
-                    
-                    Rectangle()
-                        .fill(.ultraThinMaterial)
-                        .frame(height: 2)
-                        .padding(.vertical, 10)
-                    
+                    Text(mission.launchDate?.formatted(date: .complete, time: .omitted) ?? "N/A")
+                        .font(.title3)
                     
                     VStack(alignment: .leading) {
+                        
+                        MyDivider()
+                        
                         Text("Mission Highlights")
                             .font(.title.bold())
                             .padding(.bottom, 5)
                         
                         Text(mission.description)
+                        
+                        MyDivider()
+                        
+                        Text("Crew")
+                            .font(.title.bold())
+                            .padding(.horizontal)
+                            .padding(.bottom, 5)
+                        
                     }
                     .padding(.horizontal)
+                
                     
-                    
-                    Rectangle()
-                        .fill(.ultraThinMaterial)
-                        .frame(height: 2)
-                        .padding(.vertical, 10)
-                    
-                    Text("Crew")
-                        .font(.title.bold())
-                        .padding(.horizontal)
-                        .padding(.bottom, 5)
-                        .frame(maxWidth: .infinity, alignment: .leading)
-                    
-                    
-                    ScrollView(.horizontal, showsIndicators:false) {
-                        HStack {
-                            ForEach(crew, id: \.role) {crewMember in
-                                NavigationLink {
-                                    Text("Astronaut details")
-                                } label: {
-                                    HStack {
-                                        Image(crewMember.astronaut.id)
-                                            .resizable()
-                                            .frame(width: 104, height: 72)
-                                            .clipShape(RoundedRectangle(cornerRadius: 10))
-                                            .overlay (
-                                                RoundedRectangle(cornerRadius: 10)
-                                                    .stroke(.white, lineWidth: 1)
-                                            )
-                                        VStack (alignment: .leading) {
-                                            Text(crewMember.astronaut.name)
-                                                .foregroundStyle(Color.white)
-                                                .font(.headline.bold())
-                                            
-                                            Text(crewMember.role)
-                                                .foregroundStyle(crewMember.role.hasPrefix("Command") ? Color.red.opacity(0.8) : Color.secondary)
-                                                
-                                        }
-                                        .padding(.horizontal)
-                                    }
-                                }
-                            }
-                        }
-                    }
-                    .padding(.vertical)
-                    .background(.ultraThinMaterial)
+                    CrewView(crew: crew)
                 }
             }
         }
@@ -111,7 +75,59 @@ struct MissionView: View {
     let missions: [Mission] =  Bundle.main.decode("missions.json")
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     
-    return MissionView(mission: missions[0], astronauts: astronauts)
+    return MissionView(mission: missions[1], astronauts: astronauts)
         .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
     
+}
+
+// Custom Divider
+struct MyDivider: View {
+    var body: some View {
+        Rectangle()
+            .fill(.ultraThinMaterial)
+            .frame(height: 2)
+            .padding(.vertical, 10)
+    }
+}
+
+
+// Crew View
+struct CrewView: View {
+   
+    var crew: [MissionView.CrewMember]
+    
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators:false) {
+            HStack {
+                ForEach(crew, id: \.role) {crewMember in
+                    NavigationLink {
+                        AstronautView(astronaut: crewMember.astronaut)
+                    } label: {
+                        HStack {
+                            Image(crewMember.astronaut.id)
+                                .resizable()
+                                .frame(width: 104, height: 72)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .overlay (
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(.white, lineWidth: 1)
+                                )
+                            VStack (alignment: .leading) {
+                                Text(crewMember.astronaut.name)
+                                    .foregroundStyle(Color.white)
+                                    .font(.headline.bold())
+                                
+                                Text(crewMember.role)
+                                    .foregroundStyle(crewMember.role.hasPrefix("Command") ? Color.red.opacity(0.8) : Color.secondary)
+                                
+                            }
+                            .padding(.horizontal)
+                        }
+                    }
+                }
+            }
+        }
+        .padding(.vertical)
+        .background(.ultraThinMaterial)
+    }
 }
